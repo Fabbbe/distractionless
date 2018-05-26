fb5a62a0037a7d3ea1b8ec029ac2797c154bfa38
Fabbbe <fabbbe166@gmail.com>
2018-05-26 23:57:49 +0200

    Added right-click and made it possible to open and save files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Distractionless
-Distractionless is a markdown writing application 
-that in made to distract you as little as possible, 
-so that you can give more focus to your work.
+Distractionless is a markdown writing application that in made to distract you as little as possible, so that you can focus on work.
+
+## How it works
+Distractionless is built using PyQt5. The workflow is centered around the right-click menu, although keyboard shortcuts will (hopefully) be added soon.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 '''
@@ -7,12 +7,14 @@ PyQt5
 '''
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPlainTextEdit, QHBoxLayout, QFrame, QMenu, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPlainTextEdit, QVBoxLayout, QFrame, QMenu, QFileDialog, QLineEdit
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
 WINDOW_TITLE = 'Distraction Less'
 FONT_SIZE = 14
+INFO_FONT_SIZE = 11
+MAXIMUM_WIDTH = 980
 BACKGROUND_COLOR = '#f3f3e0'
 
 class App(QMainWindow):
@@ -22,6 +24,8 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        self.working_file_name = ''
+
         self.InitUI()
         
     def InitUI(self):
@@ -38,12 +42,29 @@ class App(QMainWindow):
         self.widget.setStyleSheet('background-color:'+ BACKGROUND_COLOR +';')
 
         # Create default text font
-        self.input_font = QFont("Mono")
+        self.input_font = QFont('Mono')
         self.input_font.setPointSize(FONT_SIZE)
+
+        # Create info bar font
+        self.info_font = QFont('Mono')
+        self.info_font.setPointSize(INFO_FONT_SIZE)
+
+        # Create LineEdit for the top info bar
+        # This bar is for displaying info about
+        # the current file
+        self.top_info_bar = QLineEdit(self.widget)
+        self.top_info_bar.setReadOnly(True) 
+        self.top_info_bar.setMaximumWidth(MAXIMUM_WIDTH)
+
+        # Set font and style
+        self.top_info_bar.setStyleSheet('qproperty-alignment: AlignCenter;')
+        self.top_info_bar.setFont(self.info_font)
+        self.top_info_bar.setFrame(QFrame.NoFrame)
+        self.update_top_info_bar()
 
         # Create text feild
         self.center_text = QPlainTextEdit(self.widget)
-        self.center_text.setMaximumWidth(980)
+        self.center_text.setMaximumWidth(MAXIMUM_WIDTH)
         self.center_text.setFont(self.input_font)
 
         # Disable frame style
@@ -54,28 +75,47 @@ class App(QMainWindow):
         self.center_text.customContextMenuRequested.connect(self.context_menu_event)
 
         # Create HBox and set it as layout
-        self.widget.setLayout(QHBoxLayout())
+        self.widget.setLayout(QVBoxLayout())
 
         # Layout settings
+        self.widget.layout().addWidget(self.top_info_bar)
         self.widget.layout().addWidget(self.center_text)
         self.widget.layout().setContentsMargins(0,0,0,0) 
         self.widget.layout().setAlignment(Qt.AlignCenter)
 
         self.setCentralWidget(self.widget)
 
-    def save_text(self):
+    def save_text_file(self):
         '''
         Saves the written text to a file
-        
-        TODO: Make it work
         '''
         text_to_save = self.center_text.toPlainText()
-        name = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)")[0]
+        name = QFileDialog.getSaveFileName(self,'Save as...','','All Files (*);;Text Files (*.txt)')[0]
 
         with open(name, 'w') as save_file:
             save_file.write(text_to_save)
 
-        self.setWindowTitle(name)
+        # Show the new file path
+        self.working_file_name = name
+
+        # Update info bar
+        self.update_top_info_bar()
+
+    def open_text_file(self):
+        ''' Opens a file and '''
+        name = QFileDialog.getOpenFileName(self,'Open a File','','All Files (*);;Text Files (*.txt)')[0]
+
+        with open(name, 'r') as new_file:
+            new_text = new_file.read()
+
+        self.center_text.setPlainText(new_text)
+        
+        # Set current file name to the opened one.
+        self.working_file_name = name
+        
+        # Update info bar
+        self.update_top_info_bar()
+
 
     def context_menu_event(self, event):
         '''
@@ -88,8 +128,11 @@ class App(QMainWindow):
 
         copy_action = context_menu.addAction('Copy') # Copy to clipboard
         paste_action = context_menu.addAction('Paste') # Paste clipboard
+
         # Save file as...
         save_as_action = context_menu.addAction('Save as...') 
+        # Open file
+        open_action = context_menu.addAction('Open file')
 
         action = context_menu.exec_(self.center_text.mapToGlobal(event))
 
@@ -101,7 +144,16 @@ class App(QMainWindow):
 
         # File managment actions 
         elif action == save_as_action:
-            self.save_text()
+            self.save_text_file()
+        elif action == open_action:
+            self.open_text_file()
+
+    def update_top_info_bar(self):
+        if self.working_file_name != '':
+            self.top_info_bar.setText(self.working_file_name)
+        else:
+            self.top_info_bar.setText('New File')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
